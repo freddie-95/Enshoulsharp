01d59659fc172d7d308562c382e2df2860ca8977
Freddie <pecher.frederik@studmail.w-hs.de>
2020-04-11 15:24:05 +0200

    Update
    Freddie AIO

---
+++ Freddie AIO/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Freddie_AIO
+{
+	using EnsoulSharp.SDK;
+
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			GameEvent.OnGameLoad += OnGameLoad;
+		}
+
+		private static void OnGameLoad()
+		{
+
+		//
+
+		}
+		
+	}
+}
---
+++ Freddie AIO/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+
+[assembly: AssemblyTitle("Freddie AIO")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Freddie AIO")]
+[assembly: AssemblyCopyright("Copyright © 2020 by freddie-95")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4471ebc6-f76d-463d-9934-a4f0950d15d7")]
+
+[assembly: AssemblyVersion("1.0")]
+[assembly: AssemblyFileVersion("1.0")]
